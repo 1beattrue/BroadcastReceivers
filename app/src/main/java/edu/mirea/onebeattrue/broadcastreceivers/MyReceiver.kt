c801277c648dc850ece0d65aa2d0c12ea1e2f93d
Vitaly <onebeattrue@gmail.com>
2023-09-09 13:25:20 +0300

    Домашнее задание - отслеживание заряда батареи

--- app/src/main/java/edu/mirea/onebeattrue/broadcastreceivers/MyReceiver.kt
+++ app/src/main/java/edu/mirea/onebeattrue/broadcastreceivers/MyReceiver.kt
@@ -8,8 +8,8 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
-            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
             }
         }
     }
