b8dfc9846c9c511e4be90264170ddd63d81b10c9
Vitaly <onebeattrue@gmail.com>
2023-09-09 14:21:28 +0300

    Создание собственных широковещательных сообщений и проверка их работы

--- app/src/main/java/edu/mirea/onebeattrue/broadcastreceivers/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/broadcastreceivers/MainActivity.kt
@@ -2,20 +2,30 @@ package edu.mirea.onebeattrue.broadcastreceivers
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
+    private var clickCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.btnButton).setOnClickListener {
+            Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra(MyReceiver.EXTRA_COUNT, ++clickCount)
+                sendBroadcast(this)
+            }
+        }
+
         val intentFilter = IntentFilter().apply {
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(MyReceiver.ACTION_CLICKED)
         }
         registerReceiver(receiver, intentFilter)
     }
